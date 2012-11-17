e359c7585f383d38306264ebbc831fa6a00231a6
masbicudo <masbicudo@gmail.com>
2012-11-17 18:14:06 -0200

    #114: Melhorar os estilos da tela de "primeiro acesso do usuário".
    - estilo na mensagem que existe em cima das caixas de alterar senha
    - caixas de senha com caracteres ocultos

--- CerebelloWebRole/Areas/App/Models/PasswordViewModel.cs
+++ CerebelloWebRole/Areas/App/Models/PasswordViewModel.cs
@@ -9,14 +9,17 @@ namespace CerebelloWebRole.Areas.App.Models
 {
     public class PasswordViewModel
     {
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Informe a sua senha atual.")]
         [Display(Name = "Senha atual")]
         public string OldPassword { get; set; }
 
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Informe uma senha válida.")]
         [Display(Name = "Senha desejada")]
         public string Password { get; set; }
 
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Repita a senha.")]
         [Display(Name = "Repita a senha desejada")]
         public string RepeatPassword { get; set; }
