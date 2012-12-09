07c4c1e1f2fbf79eee6d7adf4d6a85247d4a4cbb
andrerpena <andrerpena@gmail.com>
2012-12-09 20:06:12 -0200

    - Chat agora detecta URLs e e-Mails. (foda-se que é irrelevante, eu quiz fazer)
    - Corrigido um bug no chat que fazia a imagem do usuário enviando a mensagem ficar esquisita quando ele não tinha uma imagem no Gravatar

--- CerebelloWebRole/Scripts/Controls/jquery.cerebello.chat.window.js
+++ CerebelloWebRole/Scripts/Controls/jquery.cerebello.chat.window.js
@@ -20,12 +20,33 @@
 
         this.addMessage = function (message) {
             var _this = this;
+            
+            // takes a jQuery element and replace it's content that seems like an URL with an
+            // actual link or e-mail
+            function linkify($element) {
+                var inputText = $element.html();
+                var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+                //URLs starting with http://, https://, or ftp://
+                replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+                replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+                //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+                replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+                replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+                //Change email addresses to mailto:: links.
+                replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
+                replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+                return $element.html(replacedText);
+            }
 
             // gets the last message to see if it's possible to just append the text
             var $lastMessage = $("div.chat-message:last", _this.chatContainer.$windowInnerContent);
             if ($lastMessage.length && $lastMessage.attr("data-val-user-from") == message.UserFrom.Id) {
                 // we can just append text then
-                $("<p/>").text(message.Message).appendTo($(".chat-text-wrapper", $lastMessage));
+                linkify($("<p/>").text(message.Message)).appendTo($(".chat-text-wrapper", $lastMessage));
             }
             else {
                 // in this case we need to create a whole new message
@@ -36,7 +57,8 @@
                 var $textWrapper = $("<div/>").addClass("chat-text-wrapper").appendTo($chatMessage);
 
                 // add text
-                $("<p/>").text(message.Message).appendTo($textWrapper);
+                linkify($("<p/>").text(message.Message)).appendTo($textWrapper);
+                
 
                 // add image
                 $("<img/>").attr("src", decodeURI(message.UserFrom.GravatarUrl)).appendTo($gravatarWrapper);
