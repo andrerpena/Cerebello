159c9620161e034f819ac338c8a9d4e35ff1e217
masbicudo <masbicudo@gmail.com>
2013-02-06 17:46:24 -0200

    Melhor forma de verificar se URL é local.

--- CerebelloWebRole/Code/Filters/CanonicalUrlFilter.cs
+++ CerebelloWebRole/Code/Filters/CanonicalUrlFilter.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CerebelloWebRole.Code.Filters
@@ -12,7 +9,7 @@ namespace CerebelloWebRole.Code.Filters
         public void OnActionExecuting(ActionExecutingContext filterContext)
         {
             var request = filterContext.RequestContext.HttpContext.Request;
-            if (!request.IsAjaxRequest() && request.Url.Host != "localhost" && request.Url.Host != "127.0.0.1" && request.Url.Host != ConfigurationManager.AppSettings["CanonicalUrlHost"])
+            if (!request.IsAjaxRequest() && request.Url != null && !request.Url.IsLoopback && request.Url.Host != ConfigurationManager.AppSettings["CanonicalUrlHost"])
             {
                 var uriBuilder = new UriBuilder(request.Url) { Host = ConfigurationManager.AppSettings["CanonicalUrlHost"] };
                 filterContext.Result = new RedirectResult(uriBuilder.ToString());
