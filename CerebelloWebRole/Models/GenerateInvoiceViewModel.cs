47de8961151805e7011463f8a38292e857aec5d2
masbicudo <masbicudo@gmail.com>
2013-02-28 14:19:32 -0300

    MasterAdmin: ferramenta para geração de faturas

---
+++ CerebelloWebRole/Models/GenerateInvoiceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CerebelloWebRole.App_GlobalResources;
+
+namespace CerebelloWebRole.Models
+{
+    public class GenerateInvoiceViewModel
+    {
+        [Required(ErrorMessageResourceType = typeof(ModelStrings), ErrorMessageResourceName = "RequiredValidationMessage")]
+        [Display(Name = "Identificador do consultório")]
+        public String PracticeIdentifier { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ModelStrings), ErrorMessageResourceName = "RequiredValidationMessage")]
+        [Display(Name = "Data de vencimento")]
+        public DateTime? DueDate { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ModelStrings), ErrorMessageResourceName = "RequiredValidationMessage")]
+        [Display(Name = "Valor")]
+        public decimal? Amount { get; set; }
+    }
+}
