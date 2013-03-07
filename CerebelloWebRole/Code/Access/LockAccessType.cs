41e94a461aef7fae1187d5eb3b5fbb673a35429c
masbicudo <masbicudo@gmail.com>
2013-03-07 15:22:52 -0300

    Forma melhor de colocar cadeado nas páginas usando @helper

---
+++ CerebelloWebRole/Code/Access/LockAccessType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CerebelloWebRole.Code.Access
+{
+    [Flags]
+    public enum LockAccessType
+    {
+        None = 0,
+
+        AdminOrOwner = Admin + Owner,
+        SelfOrAdminOrOwner = Self + Admin + Owner,
+
+        /// <summary>
+        /// You, the logged user.
+        /// </summary>
+        You = 1,
+
+        /// <summary>
+        /// The owner of the information.
+        /// </summary>
+        Self = 2,
+
+        /// <summary>
+        /// Any administrator.
+        /// </summary>
+        Admin = 4,
+
+        /// <summary>
+        /// The owner of the account.
+        /// </summary>
+        Owner = 8,
+    }
+}
---
+++ CerebelloWebRole/Code/Access/LockElementActionType.cs
@@ -0,0 +1,40 @@
+﻿namespace CerebelloWebRole.Code.Access
+{
+    public enum LockElementActionType
+    {
+        /// <summary>
+        /// Locks some not specified thing.
+        /// </summary>
+        Generic,
+
+        /// <summary>
+        /// Locks a specific field.
+        /// </summary>
+        Field,
+
+        /// <summary>
+        /// Locks the edition of a specific field.
+        /// </summary>
+        FieldEdit,
+
+        /// <summary>
+        /// Locks a groups of fields.
+        /// </summary>
+        Section,
+
+        /// <summary>
+        /// Locks the edition of a groups of fields.
+        /// </summary>
+        SectionEdit,
+
+        /// <summary>
+        /// Locks the access to an entire screen.
+        /// </summary>
+        ScreenAccess,
+
+        /// <summary>
+        /// Locks the access to an entire area of the software.
+        /// </summary>
+        SoftwareAreaAccess,
+    }
+}
