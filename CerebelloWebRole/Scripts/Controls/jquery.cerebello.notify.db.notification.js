288afeef781fedf8470bacb49cfa859d673489b1
André Rodrigues Pena <andrerpena@gmail.com>
2013-03-30 08:49:40 -0300

    - Introdução interativa na tela de início do consultório e na tela de início do médico
    - Mudança na mensagem de boas vindas pra ser mais informativo (notificação que o médico recebe ao criar uma conta)

--- CerebelloWebRole/Scripts/Controls/jquery.cerebello.notify.db.notification.js
+++ CerebelloWebRole/Scripts/Controls/jquery.cerebello.notify.db.notification.js
@@ -1,115 +1,123 @@
-﻿(function ($) {
+﻿function notifyMedicalAppointment(id, patientId, patientName, doctorId, doctorName, appointmentId, time, practiceIdentifier, doctorIdentifier) {
+    /// <summary>notify the SECRETARY about a patient that should have come</summary>
+    /// <param name="id" type="Number">notification id</param>
+    /// <param name="patientId" type="patient id">patient id</param>
+    /// <param name="patientName" type="String">patient name</param>
+    /// <param name="doctorId" type="id">doctor id</param>
+    /// <param name="doctorName" type="string">doctor name</param>
+    /// <param name="appointmentId" type="string">appointmentId</param>
+    /// <param name="time" type="string">appointment time spelled</param>
 
-    function notifyMedicalAppointment(id, patientId, patientName, doctorId, doctorName, appointmentId, time, practiceIdentifier, doctorIdentifier) {
-        /// <summary>notify the SECRETARY about a patient that should have come</summary>
-        /// <param name="id" type="Number">notification id</param>
-        /// <param name="patientId" type="patient id">patient id</param>
-        /// <param name="patientName" type="String">patient name</param>
-        /// <param name="doctorId" type="id">doctor id</param>
-        /// <param name="doctorName" type="string">doctor name</param>
-        /// <param name="appointmentId" type="string">appointmentId</param>
-        /// <param name="time" type="string">appointment time spelled</param>
+    var patientUrl = "/p/" + practiceIdentifier + "/d/" + doctorIdentifier + "/Patients/Details/" + patientId;
 
-        var patientUrl = "/p/" + practiceIdentifier + "/d/" + doctorIdentifier + "/Patients/Details/" + patientId;
+    var $newAppointmentPanel = $("<div/>").addClass("appointment-notification-panel");
 
-        var $newAppointmentPanel = $("<div/>").addClass("appointment-notification-panel");
+    var $patientLink = $("<a/>").addClass("patient").attr("href", patientUrl).text(patientName);
+    var $timeSpan = $("<span/>").addClass("time").text(time);
 
-        var $patientLink = $("<a/>").addClass("patient").attr("href", patientUrl).text(patientName);
-        var $timeSpan = $("<span/>").addClass("time").text(time);
+    var $text = $("<div/>").addClass("text").appendTo($newAppointmentPanel);
+    $text.append($patientLink);
+    $text.append(" possui uma consulta marcada com " + doctorName + " às ");
+    $text.append($timeSpan);
 
-        var $text = $("<div/>").addClass("text").appendTo($newAppointmentPanel);
-        $text.append($patientLink);
-        $text.append(" possui uma consulta marcada com " + doctorName + " às ");
-        $text.append($timeSpan);
+    $("<a/>").attr("href", "#").text("O paciente chegou").appendTo($newAppointmentPanel).click(function (e) {
+        e.preventDefault();
+        $(this).trigger("notification-close");
+        $.connection.notificationsHub.server.patientArrived(appointmentId, time);
+        $.connection.notificationsHub.server.closeNotification(id);
+    });
 
-        $("<a/>").attr("href", "#").text("O paciente chegou").appendTo($newAppointmentPanel).click(function (e) {
-            e.preventDefault();
+    $("<a/>").attr("href", "#").text("O paciente não irá comparecer").appendTo($newAppointmentPanel).click(function () {
+        e.preventDefault();
+        if (confirm("A consulta será considerada não realizada. Deseja continuar?")) {
             $(this).trigger("notification-close");
-            $.connection.notificationsHub.server.patientArrived(appointmentId, time);
+            $.connection.notificationsHub.server.patientWillNotArrive(appointmentId);
+            $.connection.notificationsHub.server.closeNotification(id);
+        }
+    });
+
+    $.notify($newAppointmentPanel, undefined, undefined, true, null, function () {
+        $.connection.notificationsHub.server.closeNotification(id);
+    });
+}
+
+function notifyGenericAppointment(id, text, time) {
+    /// <summary>notify the DOCTOR about an upcoming generic appointment</summary>
+    /// <param name="id" type="id">appointment id</param>
+    /// <param name="text" type="String">appointment text/description</param>
+    /// <param name="time" type="String">appointment time spelled</param>
+
+    var $newAppointmentPanel = $("<div/>").addClass("appointment-notification-panel");
+    var $timeSpan = $("<span/>").addClass("time").text(time);
+    var $descriptionSpan = $("<span/>").addClass("description").text(text);
+
+    var $text = $("<div/>").addClass("text").appendTo($newAppointmentPanel);
+    $text.append("Você possui um compromisso às ");
+    $text.append($timeSpan);
+    $text.append(": ");
+    $text.append($descriptionSpan);
+
+    $("<a/>").attr("href", "#").text("Descartar").appendTo($newAppointmentPanel).click(function (e) {
+        e.preventDefault();
+        $(this).trigger("notification-close");
+        $.connection.notificationsHub.server.closeNotification(id);
+    });
+
+    $.notify($newAppointmentPanel, undefined, undefined, false, null, function () {
+        $.connection.notificationsHub.server.closeNotification(id);
+    });
+}
+
+function notifyPatientArrived(id, patientId, patientName, time, practiceIdentifier, doctorIdentifier) {
+    /// <summary>notify the DOCTOR the patient has arrived</summary>
+    /// <param name="id" type="Number">notification id</param>
+    /// <param name="patientId" type="Number">patient id</param>
+    /// <param name="patientName" type="String">patient name</param>
+    /// <param name="time" type="String">appointment time spelled</param>
+
+    var patientUrl = "/p/" + practiceIdentifier + "/d/" + doctorIdentifier + "/Patients/Details/" + patientId;
+
+
+    var $newAppointmentPanel = $("<div/>").addClass("appointment-notification-panel");
+
+    var $patientLink = $("<a/>").addClass("patient").attr("href", patientUrl).text(patientName);
+    var $timeSpan = $("<span/>").addClass("time").text(time);
+
+    var $text = $("<div/>").addClass("text").appendTo($newAppointmentPanel);
+    $text.append($patientLink);
+    $text.append(" chegou para uma consulta às ");
+    $text.append($timeSpan);
+
+    $.notify($newAppointmentPanel, undefined, undefined, true, null, function () {
+        $.connection.notificationsHub.server.closeNotification(id);
+    });
+}
+
+function notifyNewUserCreated(id, newUserName) {
+    /// <summary>notify the user a new user has been created</summary>
+    $.notify("O usuário " + newUserName + " foi criado com successo. A senha padrão é: 123abc. O usuário terá a oportunidade de modificar esta senha no primeiro acesso.",
+        undefined, undefined, true, null, function () {
             $.connection.notificationsHub.server.closeNotification(id);
         });
+}
 
-        $("<a/>").attr("href", "#").text("O paciente não irá comparecer").appendTo($newAppointmentPanel).click(function () {
-            e.preventDefault();
-            if (confirm("A consulta será considerada não realizada. Deseja continuar?")) {
-                $(this).trigger("notification-close");
-                $.connection.notificationsHub.server.patientWillNotArrive(appointmentId);
-                $.connection.notificationsHub.server.closeNotification(id);
-            }
-        });
+function notifyCompleteInfo(id) {
+    /// <summary>notify the user a new user has been created</summary>
 
-        $.notify($newAppointmentPanel, undefined, undefined, true, null, function () {
+    var $text = $("<div/>");
+
+    $text.append($("<p>").html("Bem vindo ao Cerebello!"));
+    $text.append($("<p>").html("Antes de começar, é importante que você complete as informações sobre seu usuário e seu consultório."));
+    $text.append($("<p>").html("Para agendar e realizar consultas, assim como visualizar os perfis dos pacientes, é necessário selecionar um médico primeiro. Selecione um na seção 'Médicos do consultório'."));
+    $text.append($("<p>").html("Algumas telas possuem um menu lateral chamado 'Introdução interativa' onde é possível ter uma visão geral da tela. Experimente!"));
+
+    $.notify($text,
+        undefined, undefined, true, null, function () {
             $.connection.notificationsHub.server.closeNotification(id);
         });
-    }
+}
 
-    function notifyGenericAppointment(id, text, time) {
-        /// <summary>notify the DOCTOR about an upcoming generic appointment</summary>
-        /// <param name="id" type="id">appointment id</param>
-        /// <param name="text" type="String">appointment text/description</param>
-        /// <param name="time" type="String">appointment time spelled</param>
-        
-        var $newAppointmentPanel = $("<div/>").addClass("appointment-notification-panel");
-        var $timeSpan = $("<span/>").addClass("time").text(time);
-        var $descriptionSpan = $("<span/>").addClass("description").text(text);
-
-        var $text = $("<div/>").addClass("text").appendTo($newAppointmentPanel);
-        $text.append("Você possui um compromisso às ");
-        $text.append($timeSpan);
-        $text.append(": ");
-        $text.append($descriptionSpan);
-
-        $("<a/>").attr("href", "#").text("Descartar").appendTo($newAppointmentPanel).click(function (e) {
-            e.preventDefault();
-            $(this).trigger("notification-close");
-            $.connection.notificationsHub.server.closeNotification(id);
-        });
-
-        $.notify($newAppointmentPanel, undefined, undefined, false, null, function () {
-            $.connection.notificationsHub.server.closeNotification(id);
-        });
-    }
-
-    function notifyPatientArrived(id, patientId, patientName, time, practiceIdentifier, doctorIdentifier) {
-        /// <summary>notify the DOCTOR the patient has arrived</summary>
-        /// <param name="id" type="Number">notification id</param>
-        /// <param name="patientId" type="Number">patient id</param>
-        /// <param name="patientName" type="String">patient name</param>
-        /// <param name="time" type="String">appointment time spelled</param>
-
-        var patientUrl = "/p/" + practiceIdentifier + "/d/" + doctorIdentifier + "/Patients/Details/" + patientId;
-
-
-        var $newAppointmentPanel = $("<div/>").addClass("appointment-notification-panel");
-
-        var $patientLink = $("<a/>").addClass("patient").attr("href", patientUrl).text(patientName);
-        var $timeSpan = $("<span/>").addClass("time").text(time);
-
-        var $text = $("<div/>").addClass("text").appendTo($newAppointmentPanel);
-        $text.append($patientLink);
-        $text.append(" chegou para uma consulta às ");
-        $text.append($timeSpan);
-
-        $.notify($newAppointmentPanel, undefined, undefined, true, null, function () {
-            $.connection.notificationsHub.server.closeNotification(id);
-        });
-    }
-
-    function notifyNewUserCreated(id, newUserName) {
-        /// <summary>notify the user a new user has been created</summary>
-        $.notify("O usuário " + newUserName + " foi criado com successo. A senha padrão é: 123abc. O usuário terá a oportunidade de modificar esta senha no primeiro acesso.",
-            undefined, undefined, true, null, function () {
-                $.connection.notificationsHub.server.closeNotification(id);
-            });
-    }
-    
-    function notifyCompleteInfo(id) {
-        /// <summary>notify the user a new user has been created</summary>
-        $.notify("Por favor, complete as informações do seu usuário e consultório em configurações",
-            undefined, undefined, true, null, function () {
-                $.connection.notificationsHub.server.closeNotification(id);
-            });
-    }
+(function ($) {
 
     $.connection.notificationsHub.client.notify = function (id, type, data) {
         switch (type) {
