f7d198b0e2e23b55e4e03b9adecde3dca725d9e9
masbicudo <masbicudo@gmail.com>
2013-04-12 00:38:52 -0300

    Preço de médicos adicionais estava errado. Correto = 29. (metade do valor da assinatura)

--- CerebelloWebRole/Code/Bus.cs
+++ CerebelloWebRole/Code/Bus.cs
@@ -61,35 +61,8 @@ namespace CerebelloWebRole.Code
             /// <summary> Equals 15 </summary>
             public const int DISCOUNT_YEAR = 15;
 
-            /// <summary> Equals 39 </summary>
-            public const int DOCTOR_PRICE = 39;
-        }
-
-        /// <summary>
-        /// Old professional plan constants.
-        /// </summary>
-        public static class ProOld
-        {
-            /// <summary> Equals 119 </summary>
-            public const int PRICE_MONTH = 119;
-
-            /// <summary> Equals 339 (113 × 3) </summary>
-            public const int PRICE_QUARTER = 3 * 113;
-
-            /// <summary> Equals 642 (107 × 6) </summary>
-            public const int PRICE_SEMESTER = 6 * 107;
-
-            /// <summary> Equals 1068 (89 × 12) </summary>
-            public const int PRICE_YEAR = 12 * 89;
-
-            /// <summary> Equals 5 </summary>
-            public const int DISCOUNT_QUARTER = 5;
-
-            /// <summary> Equals 10 </summary>
-            public const int DISCOUNT_SEMESTER = 10;
-
-            /// <summary> Equals 25 </summary>
-            public const int DISCOUNT_YEAR = 25;
+            /// <summary> Equals 29 </summary>
+            public const int DOCTOR_PRICE = (int)(PRICE_MONTH / 2);
         }
     }
 }
