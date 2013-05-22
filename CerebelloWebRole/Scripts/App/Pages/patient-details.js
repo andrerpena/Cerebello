042a370c7d7d7ed3c6f06c29f1dd37d3c09c773c
masbicudo <masbicudo@gmail.com>
2013-05-22 18:17:25 -0300

    JS e CSS do upload de arquivos movidos para arquivos externos

--- CerebelloWebRole/Scripts/App/Pages/patient-details.js
+++ CerebelloWebRole/Scripts/App/Pages/patient-details.js
@@ -1,1 +1,34 @@
-﻿
+﻿$(function () {
+    'use strict';
+
+    // Preventing browser default behavior when drag and dropping.
+    $(document).bind('drop dragover', function (e) {
+        e.preventDefault();
+    });
+
+    $(document).bind('dragover', function (e) {
+        var dropZone = $('.dropzone'),
+            timeout = window.dropZoneTimeout;
+        
+        dropZone.addClass('fade');
+        
+        if (!timeout) {
+            dropZone.addClass('in');
+        } else {
+            clearTimeout(timeout);
+        }
+        
+        dropZone.each(function (name, value) {
+            if (e.target === value || jQuery.contains(value, e.target)) {
+                $(value).addClass('hover');
+            } else {
+                $(value).removeClass('hover');
+            }
+        });
+        
+        window.dropZoneTimeout = setTimeout(function () {
+            dropZone.removeClass('in hover');
+            window.dropZoneTimeout = null;
+        }, 100);
+    });
+});
