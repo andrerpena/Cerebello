01277e70cf8e42d790a53a3b11975385136907a5
André Rodrigues Pena <andrerpena@gmail.com>
2013-05-25 09:56:23 -0300

    Algumas mudanças no chat

--- CerebelloWebRole/Code/Chat/ChatUser.cs
+++ CerebelloWebRole/Code/Chat/ChatUser.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CerebelloWebRole.Code.Chat
+﻿namespace CerebelloWebRole.Code.Chat
 {
     public class ChatUser
     {
@@ -28,22 +23,11 @@ namespace CerebelloWebRole.Code.Chat
         /// </summary>
         public string Name { get; set; }
 
-        /// <summary>
-        /// Profile Url
-        /// </summary>
-        public string Url { get; set; }
-
         public string ProfilePictureUrl { get; set; }
 
         /// <summary>
         /// The user's status
         /// </summary>
         public StatusType Status { get; set; }
-
-        /// <summary>
-        /// Last time (UTC) this user has been active. Being active here does not mean send messages or something.
-        /// Being active means having an open browser requesting the server.
-        /// </summary>
-        public DateTime? LastActiveOn { get; set; }
     }
 }
