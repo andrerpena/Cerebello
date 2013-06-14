047c830d3e52201b6936222cca180cf66fd7a3ca
masbicudo <masbicudo@gmail.com>
2013-06-14 05:53:58 -0300

    Aplicação para migrar dados entre as contas.

---
+++ Cerebello.Manager/FormCopyAccount.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CerebelloWebRole.Code;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage.Blob;
+using CloudBlockBlob = Microsoft.WindowsAzure.Storage.Blob.CloudBlockBlob;
+using CloudStorageAccount = Microsoft.WindowsAzure.Storage.CloudStorageAccount;
+
+namespace Cerebello.Manager
+{
+    public partial class FormCopyAccount : Form
+    {
+        public FormCopyAccount()
+        {
+            InitializeComponent();
+        }
+
+        private static IEnumerable<T> SelectManyRecursive<T>(IEnumerable<T> items, Func<T, IEnumerable<T>> childrenGetter)
+        {
+            foreach (var item in items)
+            {
+                yield return item;
+
+                if (childrenGetter != null)
+                    foreach (var child in childrenGetter(item) ?? Enumerable.Empty<T>())
+                        yield return child;
+            }
+        }
+
+        private void buttonCopyStorage_Click(object sender, EventArgs e)
+        {
+            foreach (var control in SelectManyRecursive(this.Controls.OfType<Control>(), c => c.Controls.OfType<Control>()))
+                if (control is Button)
+                    this.Invoke((Action)(() => (control as Button).Enabled = false));
+
+            var task = new Task(this.CopyBlobs);
+            task.Start();
+            task.ContinueWith(t =>
+                {
+                    foreach (var control in SelectManyRecursive(this.Controls.OfType<Control>(), c => c.Controls.OfType<Control>()))
+                        if (control is Button)
+                            this.Invoke((Action)(() => (control as Button).Enabled = true));
+                });
+        }
+
+        private void buttoCopyDatabase_Click(object sender, EventArgs e)
+        {
+            foreach (var control in SelectManyRecursive(this.Controls.OfType<Control>(), c => c.Controls.OfType<Control>()))
+                if (control is Button)
+                    this.Invoke((Action)(() => (control as Button).Enabled = false));
+
+            var task = new Task(this.CopyDatabase);
+            task.Start();
+            task.ContinueWith(t =>
+            {
+                foreach (var control in SelectManyRecursive(this.Controls.OfType<Control>(), c => c.Controls.OfType<Control>()))
+                    if (control is Button)
+                        this.Invoke((Action)(() => (control as Button).Enabled = true));
+            });
+        }
+
+        private void CopyDatabase()
+        {
+            this.Invoke((Func<object, int>)this.listBoxItems.Items.Add, "Copying database");
+
+            string exportBlobPath;
+
+            var storageKeyCerebello2 = "DW8oW7HSDj2jQ+uz4p+elMpC6B6/v/WVoJ918PPZiCMOwALIVOIY/AKLd/WvMGdWFi7vlarPvri1eS6KduowKA==";
+
+            {
+                // Set Inputs to the REST Requests in the helper class.
+                var helperToExport = new ImportExportHelper
+                    {
+                        // Set Inputs to the REST Requests in the helper class.
+                        EndPointUri = ImportExportHelper.EndPoints.EastUS,
+
+                        // this is the destination storage account key: cerebello2
+                        StorageKey = storageKeyCerebello2,
+
+                        // this is the source database account: cerebellohq
+                        DatabaseServerName = "kurptcva75.database.windows.net",
+                        DatabaseName = "cerebellohq",
+                        DatabaseServerUserName = "cerebello",
+                        DatabaseServerPassword = "26uj27oP",
+                    };
+
+                // Export database to a blob in the destination account.
+                exportBlobPath = helperToExport.DoExport(
+                    string.Format(
+                        @"https://cerebello2.blob.core.windows.net/db-backups/{0}-{1}.bacpac",
+                        helperToExport.DatabaseName,
+                        DateTime.UtcNow.ToString("yyyy'-'MM'-'dd'-'HH'-'mm")));
+
+                if (string.IsNullOrWhiteSpace(exportBlobPath))
+                    this.Invoke((Func<object, int>)this.listBoxItems.Items.Add, "Export database failed.");
+                else
+                    this.Invoke((Func<object, int>)this.listBoxItems.Items.Add, string.Format("Database exported to: {0}.", exportBlobPath));
+            }
+
+            if (!string.IsNullOrWhiteSpace(exportBlobPath))
+            {
+                var helperToImport = new ImportExportHelper
+                    {
+                        // Set Inputs to the REST Requests in the helper class.
+                        EndPointUri = ImportExportHelper.EndPoints.EastUS,
+
+                        // this is the destination storage account key: cerebello2
+                        StorageKey = storageKeyCerebello2,
+
+                        DatabaseServerName = "kurptcva75.database.windows.net",
+                        DatabaseServerUserName = "cerebello",
+                        DatabaseServerPassword = "26uj27oP",
+                        DatabaseName = "cerebello2",
+                    };
+
+                // Proceed with Import operation using bacpac created by Export if the Export operation succeeded
+                var importSucceeded = helperToImport.DoImport(exportBlobPath);
+
+                this.Invoke((Func<object, int>)this.listBoxItems.Items.Add, importSucceeded ? "Import database succeded." : "Import database failed.");
+            }
+        }
+
+        private void CopyBlobs()
+        {
+            this.Invoke((Func<object, int>)this.listBoxItems.Items.Add, "Copying blobs");
+
+            var startTime = DateTime.Now;
+            var copyingItems = 0;
+            var failedItems = 0;
+
+            var srcAccount = GetStorageAccountFromConfiguration("StorageConnectionString_cerebellohq");
+            var dstAccount = GetStorageAccountFromConfiguration("StorageConnectionString_cerebello2");
+
+            var srcBlobClient = srcAccount.CreateCloudBlobClient();
+            var dstBlobClient = dstAccount.CreateCloudBlobClient();
+
+            var queueOfPendingOps = new Queue<Action>(1000);
+
+            foreach (var srcCloudBlobContainer in srcBlobClient.ListContainers())
+            {
+                var dstCloudBlobContainer = dstBlobClient
+                    .GetContainerReference(srcCloudBlobContainer.Name);
+
+                dstCloudBlobContainer.CreateIfNotExists();
+
+                foreach (var srcBlob in srcCloudBlobContainer.ListBlobs(useFlatBlobListing: true))
+                {
+                    if (srcBlob is CloudBlockBlob)
+                    {
+                        var srcBlockBlock = (CloudBlockBlob)srcBlob;
+                        var dstBlockBlock = dstCloudBlobContainer
+                            .GetBlockBlobReference(srcBlockBlock.Name);
+
+                        // Assuming the source blob container ACL is "Private", let's create a Shared Access Signature with
+                        // Expiry Time = Current Time (UTC) + 7 Days - 7 days is the maximum time allowed for copy operation to finish.
+                        // Permission = Read so that copy service can read the blob from source
+                        var sas = srcBlockBlock.GetSharedAccessSignature(
+                            new SharedAccessBlobPolicy
+                                {
+                                    SharedAccessExpiryTime = DateTime.UtcNow.AddDays(1),
+                                    Permissions = SharedAccessBlobPermissions.Read,
+                                });
+
+                        // Create a SAS URI for the blob
+                        var srcBlockBlobSasUri = string.Format("{0}{1}", srcBlockBlock.Uri, sas);
+
+                        var asyncResult = dstBlockBlock.BeginStartCopyFromBlob(
+                            new Uri(srcBlockBlobSasUri),
+                            null,
+                            null);
+
+                        queueOfPendingOps.Enqueue(
+                            () =>
+                            {
+                                bool ok = false;
+                                try
+                                {
+                                    dstBlockBlock.EndStartCopyFromBlob(asyncResult);
+                                    ok = true;
+                                }
+                                catch
+                                {
+                                }
+
+                                if (ok)
+                                {
+                                    Interlocked.Increment(ref copyingItems);
+                                    this.Invoke((Action)(() => this.labelCopied.Text = string.Format("Copying: {0} blobs", copyingItems)));
+                                }
+                                else
+                                {
+                                    Interlocked.Increment(ref failedItems);
+                                    this.Invoke((Action)(() => this.labelFailed.Text = string.Format("Failed: {0} blobs", failedItems)));
+                                    this.Invoke((Func<object, int>)this.listBoxItems.Items.Add, string.Format("Failed blob copy: {0}", dstBlockBlock.Uri));
+                                }
+                            });
+                    }
+                    else if (srcBlob is CloudPageBlob)
+                    {
+                        throw new NotImplementedException();
+                    }
+                    else if (srcBlob is CloudBlobDirectory)
+                    {
+                        // Nothing to do... directories are virtual things in azure blob storage.
+                    }
+
+                    // Don't let the queue of pending operations get too large...
+                    // otherwise this could cause copy operations to time-out.
+                    if (queueOfPendingOps.Count > 3)
+                        while (queueOfPendingOps.Count > 1)
+                            queueOfPendingOps.Dequeue()();
+                }
+            }
+
+            while (queueOfPendingOps.Count > 0)
+                queueOfPendingOps.Dequeue()();
+
+            var endTime = DateTime.Now;
+
+            this.Invoke((Func<object, int>)this.listBoxItems.Items.Add, string.Format("Todos os {1} itens foram copiados em {0}.", endTime - startTime, copyingItems));
+        }
+
+        private static CloudStorageAccount GetStorageAccountFromConfiguration(string name)
+        {
+            var storageAccountStr = StringHelper.FirstNonEmpty(
+                () => CloudConfigurationManager.GetSetting(name),
+                () => ConfigurationManager.ConnectionStrings[name].ConnectionString,
+                () => ConfigurationManager.AppSettings[name],
+                () => { throw new Exception("No storage connection string found."); });
+
+            storageAccountStr = Regex.Replace(storageAccountStr, @"\s+", m => m.Value.Contains("\n") ? "" : m.Value);
+
+            var storageAccount = CloudStorageAccount.Parse(storageAccountStr);
+            return storageAccount;
+        }
+    }
+}
---
+++ Cerebello.Manager/ImportExportHelper.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Web;
+using Cerebello.Manager.SqlAzure.ImportExport;
+using Cerebello.Manager;
+using System.Runtime.Serialization;
+
+namespace Cerebello.Manager
+{
+    class ImportExportHelper
+    {
+        public string EndPointUri { get; set; }
+        public string StorageKey { get; set; }
+        public string BlobUri { get; set; }
+        public string DatabaseServerName { get; set; }
+        public string DatabaseName { get; set; }
+        public string DatabaseServerUserName { get; set; }
+        public string DatabaseServerPassword { get; set; }
+
+        public ImportExportHelper()
+        {
+            EndPointUri = "";
+            this.DatabaseServerName = "";
+            StorageKey = "";
+            this.DatabaseName = "";
+            this.DatabaseServerUserName = "";
+            this.DatabaseServerPassword = "";
+        }
+
+        public string DoExport(string blobUri)
+        {
+            Console.Write(String.Format("Starting Export Operation - {0}\n\r", DateTime.Now));
+            string requestGuid = null;
+            bool exportComplete = false;
+            string exportedBlobPath = null;
+
+            //Setup Web Request for Export Operation
+            WebRequest webRequest = WebRequest.Create(this.EndPointUri + @"/Export");
+            webRequest.Method = WebRequestMethods.Http.Post;
+            webRequest.ContentType = @"application/xml";
+
+            //Create Web Request Inputs - Blob Storage Credentials and Server Connection Info
+            ExportInput exportInputs = new ExportInput
+            {
+                BlobCredentials = new BlobStorageAccessKeyCredentials
+                {
+                    StorageAccessKey = this.StorageKey,
+                    Uri = String.Format(blobUri, this.DatabaseName, DateTime.UtcNow.Ticks.ToString())
+                },
+                ConnectionInfo = new ConnectionInfo
+                {
+                    ServerName = this.DatabaseServerName,
+                    DatabaseName = this.DatabaseName,
+                    UserName = this.DatabaseServerUserName,
+                    Password = this.DatabaseServerPassword
+                }
+            };
+
+            //Perform Web Request
+            Console.WriteLine("Making Web Request For Export Operation...");
+            Stream webRequestStream = webRequest.GetRequestStream();
+            DataContractSerializer dataContractSerializer = new DataContractSerializer(exportInputs.GetType());
+            dataContractSerializer.WriteObject(webRequestStream, exportInputs);
+            webRequestStream.Close();
+
+            //Get Response and Extract Request Identifier
+            Console.WriteLine("Reading Response and extracting Export Request Identifier...");
+            WebResponse webResponse = null;
+            XmlReader xmlStreamReader = null;
+
+            try
+            {
+                //Initialize the WebResponse to the response from the WebRequest
+                webResponse = webRequest.GetResponse();
+
+                xmlStreamReader = XmlReader.Create(webResponse.GetResponseStream());
+                xmlStreamReader.ReadToFollowing("guid");
+                requestGuid = xmlStreamReader.ReadElementContentAsString();
+                Console.WriteLine(String.Format("Your Export Request Guid is: {0}", requestGuid));
+
+                //Get Export Operation Status
+                while (!exportComplete)
+                {
+                    Console.WriteLine("Checking export status...");
+                    List<StatusInfo> statusInfoList = CheckRequestStatus(requestGuid);
+                    Console.WriteLine(statusInfoList.FirstOrDefault().Status);
+
+                    if (statusInfoList.FirstOrDefault().Status == "Failed")
+                    {
+                        Console.WriteLine(String.Format("Database export failed: {0}", statusInfoList.FirstOrDefault().ErrorMessage));
+                        exportComplete = true;
+                    }
+
+                    if (statusInfoList.FirstOrDefault().Status == "Completed")
+                    {
+                        exportedBlobPath = statusInfoList.FirstOrDefault().BlobUri;
+                        Console.WriteLine(String.Format("Export Complete - Database exported to: {0}\n\r", exportedBlobPath));
+                        exportComplete = true;
+                    }
+                }
+                return exportedBlobPath;
+            }
+            catch (WebException responseException)
+            {
+                Console.WriteLine("Request Falied:{0}", responseException.Message);
+                if (responseException.Response != null)
+                {
+                    Console.WriteLine("Status Code: {0}", ((HttpWebResponse)responseException.Response).StatusCode);
+                    Console.WriteLine("Status Description: {0}\n\r", ((HttpWebResponse)responseException.Response).StatusDescription);
+                }
+                return null;
+            }
+        }
+
+        public bool DoImport(string blobUri)
+        {
+            Console.Write(String.Format("Starting Import Operation - {0}\n\r", DateTime.Now));
+            string requestGuid = null;
+            bool importComplete = false;
+
+            //Setup Web Request for Import Operation
+            WebRequest webRequest = WebRequest.Create(this.EndPointUri + @"/Import");
+            webRequest.Method = WebRequestMethods.Http.Post;
+            webRequest.ContentType = @"application/xml";
+
+            //Create Web Request Inputs - Database Size & Edition, Blob Store Credentials and Server Connection Info
+            ImportInput importInputs = new ImportInput
+            {
+                AzureEdition = "Web",
+                DatabaseSizeInGB = 1,
+                BlobCredentials = new BlobStorageAccessKeyCredentials
+                {
+                    StorageAccessKey = this.StorageKey,
+                    Uri = String.Format(blobUri, this.DatabaseName, DateTime.UtcNow.Ticks.ToString())
+                },
+                ConnectionInfo = new ConnectionInfo
+                {
+                    ServerName = this.DatabaseServerName,
+                    DatabaseName = this.DatabaseName,
+                    UserName = this.DatabaseServerUserName,
+                    Password = this.DatabaseServerPassword
+                }
+            };
+
+            //Perform Web Request
+            Console.WriteLine("Making Web Request for Import Operation...");
+            Stream webRequestStream = webRequest.GetRequestStream();
+            DataContractSerializer dataContractSerializer = new DataContractSerializer(importInputs.GetType());
+            dataContractSerializer.WriteObject(webRequestStream, importInputs);
+            webRequestStream.Close();
+
+            //Get Response and Extract Request Identifier
+            Console.WriteLine("Serializing response and extracting guid...");
+            WebResponse webResponse = null;
+            XmlReader xmlStreamReader = null;
+
+            try
+            {
+                //Initialize the WebResponse to the response from the WebRequest
+                webResponse = webRequest.GetResponse();
+
+                xmlStreamReader = XmlReader.Create(webResponse.GetResponseStream());
+                xmlStreamReader.ReadToFollowing("guid");
+                requestGuid = xmlStreamReader.ReadElementContentAsString();
+                Console.WriteLine(String.Format("Request Guid: {0}", requestGuid));
+
+                //Get Status of Import Operation
+                while (!importComplete)
+                {
+                    Console.WriteLine("Checking status of Import...");
+                    List<StatusInfo> statusInfoList = CheckRequestStatus(requestGuid);
+                    Console.WriteLine(statusInfoList.FirstOrDefault().Status);
+
+                    if (statusInfoList.FirstOrDefault().Status == "Failed")
+                    {
+                        Console.WriteLine(String.Format("Database import failed: {0}", statusInfoList.FirstOrDefault().ErrorMessage));
+                        importComplete = true;
+                    }
+
+                    if (statusInfoList.FirstOrDefault().Status == "Completed")
+                    {
+                        Console.WriteLine(String.Format("Import Complete - Database imported to: {0}\n\r", statusInfoList.FirstOrDefault().DatabaseName));
+                        importComplete = true;
+                    }
+                }
+                return importComplete;
+            }
+            catch (WebException responseException)
+            {
+                Console.WriteLine("Request Falied: {0}", responseException.Message);
+                {
+                    Console.WriteLine("Status Code: {0}", ((HttpWebResponse)responseException.Response).StatusCode);
+                    Console.WriteLine("Status Description: {0}\n\r", ((HttpWebResponse)responseException.Response).StatusDescription);
+                }
+
+                return importComplete;
+            }
+        }
+
+        public List<StatusInfo> CheckRequestStatus(string requestGuid)
+        {
+            WebRequest webRequest = WebRequest.Create(this.EndPointUri + string.Format("/Status?servername={0}&username={1}&password={2}&reqId={3}",
+                    HttpUtility.UrlEncode(this.DatabaseServerName),
+                    HttpUtility.UrlEncode(this.DatabaseServerUserName),
+                    HttpUtility.UrlEncode(this.DatabaseServerPassword),
+                    HttpUtility.UrlEncode(requestGuid)));
+
+            webRequest.Method = WebRequestMethods.Http.Get;
+            webRequest.ContentType = @"application/xml";
+            WebResponse webResponse = webRequest.GetResponse();
+            XmlReader xmlStreamReader = XmlReader.Create(webResponse.GetResponseStream());
+            DataContractSerializer dataContractSerializer = new DataContractSerializer(typeof(List<StatusInfo>));
+
+            return (List<StatusInfo>)dataContractSerializer.ReadObject(xmlStreamReader, true);
+        }
+
+        /// <summary>
+        /// Known DACWebService end points.
+        /// </summary>
+        public static class EndPoints
+        {
+            public static readonly string NorthCentralUS = "https://ch1prod-dacsvc.azure.com/DACWebService.svc";
+            public static readonly string SouthCentralUS = "https://sn1prod-dacsvc.azure.com/DACWebService.svc";
+            public static readonly string NorthEurope = "https://db3prod-dacsvc.azure.com/DACWebService.svc";
+            public static readonly string WestEurope = "https://am1prod-dacsvc.azure.com/DACWebService.svc";
+            public static readonly string EastAsia = "https://hkgprod-dacsvc.azure.com/DACWebService.svc";
+            public static readonly string SoutheastAsia = "https://sg1prod-dacsvc.azure.com/DACWebService.svc";
+            public static readonly string EastUS = "https://bl2prod-dacsvc.azure.com/DACWebService.svc";
+            public static readonly string WestUS = "https://by1prod-dacsvc.azure.com/DACWebService.svc";
+        }
+    }
+}
