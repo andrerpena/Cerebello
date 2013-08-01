1af1fbae77940a229d7d40424a8172ab5f05961e
André Rodrigues Pena <andrerpena@gmail.com>
2013-08-01 00:32:56 -0300

    Continuing translations

--- CerebelloWebRole/Areas/App/Models/HealthInsuranceViewModel.cs
+++ CerebelloWebRole/Areas/App/Models/HealthInsuranceViewModel.cs
@@ -8,24 +8,24 @@ namespace CerebelloWebRole.Areas.App.Models
     {
         public int? Id { get; set; }
 
-        [Display(Name = "Nome")]
+        [Display(Name = "Name")]
         [Required(ErrorMessageResourceType = typeof(ModelStrings), ErrorMessageResourceName = "RequiredValidationMessage")]
         [StringLength(Constants.DB_NAME_MAX_LENGTH, ErrorMessageResourceName = "MaxLengthValidationMessage")]
         public string Name { get; set; }
 
-        [Display(Name = "Valor da nova consulta")]
+        [Display(Name = "Medical appointment cost")]
         public decimal? NewAppointmentValue { get; set; }
 
-        [Display(Name = "Valor da consulta de retorno")]
+        [Display(Name = "Returning medical appointment cost")]
         public decimal? ReturnAppointmentValue { get; set; }
 
-        [Display(Name = "Intervalo de retorno em dias", Description = "Intervalo no qual uma consulta subsequente é considerada como sendo retorno pelo convênio médico.")]
+        [Display(Name = "Return interval in days", Description = "Interval in which, if the patient returns, the consultation is considered a return")]
         public int? ReturnDaysInterval { get; set; }
 
-        [Display(Name = "Está ativo?", Description = "Indica se novas consultas podem ser marcadas usando este convênio.")]
+        [Display(Name = "Active", Description = "Indicates whether this health insurance is active")]
         public bool IsActive { get; set; }
 
-        [Display(Name = "É particular?", Description = "Indica se é uma forma de atendimento particular, ao invés de um atendimento conveniado.")]
+        [Display(Name = "Private pay", Description = "Indicates that the consultation is payed by the patient")]
         public bool IsParticular { get; set; }
     }
 }
